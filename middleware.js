53c3ebdae6af242bba8c663bccba6b51bf0dd5c6
Harrykumarr <harikumarreddy1221@gmail.com>
2025-11-01 20:07:17 +0530

    chore: remove login page and handle redirection in middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,21 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+  const url = req.nextUrl.clone();
+  const session = req.cookies.get('next-auth.session-token');
+
+  if (url.pathname === '/') {
+    if (session) {
+      url.pathname = '/dashboard';
+      return NextResponse.redirect(url);
+    } else {
+      url.pathname = '/api/auth/signin';
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
+}
 
 export const config = { 
   matcher: [
